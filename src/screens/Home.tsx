ae157928008edbad7bb752832e1442bdee9abbfc
Goutam Das <goutam@stockedge.com>
2021-04-09 20:33:52 +0530

    Init Commit

---
+++ src/screens/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Home: React.FC = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+
+const style = StyleSheet.create({});
+
+export default Home;
